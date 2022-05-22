c206cdb086f484aa0f4e53a0eb5bda0e85e7df9f
Chetan Agarwal <chetan.mouselangelo@gmail.com>
2022-05-22 06:32:39 +0200

    Add list header

--- views/ComponentsScreen.tsx
+++ views/ComponentsScreen.tsx
@@ -1,7 +1,12 @@
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { FlatList, StyleSheet, Text, TouchableOpacity } from "react-native";
-
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import routeNames from "../routeNames";
 
 const excludeRoutes = ["Components", "Color"];
@@ -19,16 +24,32 @@ const ComponentsScreen = ({ navigation }: NativeStackScreenProps<any>) => {
         <TouchableOpacity
           style={styles.cellWrapper}
           onPress={() => navigation.navigate(item)}>
-          <Text style={styles.cellTitle}>{item}</Text>
+          <Text style={styles.text}>{item}</Text>
         </TouchableOpacity>
       )}
+      ListHeaderComponent={() => <Header />}
     />
   );
 };
 
+const Header = () => (
+  <View style={styles.header}>
+    <Text style={styles.headerText}>React-Native Components</Text>
+  </View>
+);
+
 const styles = StyleSheet.create({
   list: {
     padding: 20,
+  },
+  header: {
+    padding: 12,
+    marginBottom: 16,
+  },
+  headerText: {
+    fontSize: 24,
+    color: "#222",
+    fontWeight: "500",
   },
   cellWrapper: {
     backgroundColor: "white",
@@ -40,8 +61,8 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.2,
   },
-  cellTitle: {
-    fontSize: 16,
+  text: {
+    fontSize: 18,
     color: "#222",
   },
 });
